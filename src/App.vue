c525f72e5302b6605c16c2565da82ef9b3e6f1ac
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-07-29 00:24:59 -0300

    App vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
     <span>Hello World</span>
+    <div v-if="transactions.length > 0">
+      <span> Transactions</span>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
+  computed: {
+    ...mapGetters({
+      transactions: 'allTransactions',
+    }),
+  },
   created() {
     this.$store.dispatch('initAddon');
     // eslint-disable-next-line
-    console.debug(this.$store);
+    this.$store.dispatch('getTransactionsList', null, { root: true });
   },
 };
 </script>
