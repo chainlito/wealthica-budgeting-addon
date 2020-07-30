e3edae39ef1fa8878a1dc335b6a8b56a9c252108
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-07-30 18:02:54 -0300

    Spending chart update

--- src/components/spending/SpendingChart.vue
+++ src/components/spending/SpendingChart.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
     <div v-if="categories.length > 0">
-      <D3PieChart :config="chart_config" :datum="categories">
-      </D3PieChart>
+      <D3PieChart :config="chart_config" :datum="categories" />
     </div>
   </div>
 </template>
@@ -21,20 +20,72 @@ export default {
   },
   data() {
     return {
+      chart_data: [{
+        amount: 1860.22,
+        category: 'Home Improvement',
+        transactions: [],
+      }, {
+        amount: 1447.3600000000001,
+        category: 'General Merchandise',
+        transactions: [],
+      }, {
+        amount: 2651,
+        category: '',
+        transactions: [],
+      }, {
+        amount: 531.07,
+        category: 'Other Expenses',
+        transactions: [],
+      }, {
+        amount: 1459.2200000000003,
+        category: 'Electronics',
+        transactions: [],
+      }, {
+        amount: 224.57,
+        category: 'Clothing/Shoes',
+        transactions: [],
+      }, {
+        amount: 95.43,
+        category: 'Groceries',
+        transactions: [],
+      }, {
+        amount: 160.49,
+        category: 'Entertainment',
+        transactions: [],
+      }, {
+        amount: 500.08,
+        category: 'Uncategorized',
+        transactions: [],
+      }],
       chart_config: {
         key: 'category',
         value: 'amount',
         radius: {
-          inner: 80,
-          outter: 90,
+          inner: 135,
+          outter: 150,
           padding: 0.03,
           round: 20,
         },
         color: {
           scheme: 'schemePaired',
         },
+        transition: {
+          duration: 0,
+        },
       },
     };
   },
 };
 </script>
+
+<style lang="scss">
+.chart {
+  &__label {
+    opacity: 0 !important;
+  }
+
+  &__line {
+    opacity: 0 !important;
+  }
+}
+</style>
