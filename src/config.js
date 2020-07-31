464ce77ba06424f470ea93cec63c9a8ace53676b
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-07-31 18:31:55 -0300

    config widget

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Polyglot from 'vue-polyglot';
-import App from './App.vue';
+import App from './components/ConfigWidget.vue';
 
 Vue.config.productionTip = false;
 Vue.use(Polyglot, {
