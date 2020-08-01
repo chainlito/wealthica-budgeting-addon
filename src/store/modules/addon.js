d0ddbe6709fe3ab272027cc22985cf5ef0fc632d
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-07-31 23:38:51 -0300

    addon options data

--- src/store/modules/addon.js
+++ src/store/modules/addon.js
@@ -22,12 +22,15 @@ const getters = {
 // actions
 const actions = {
   initAddon({
-    commit, /* dispatch, getters, */ state,
+    commit, dispatch, getters, state,
   }) {
     const updateOptions = (options) => {
       const newOptions = _.merge({}, state.addonOptions, options);
       commit(types.UPDATE_ADDON_OPTIONS, { data: newOptions });
-      // dispatch('updateBudgeting', getters.data.coins, { root: true });
+
+      // eslint-disable-next-line
+      console.log(getters.data, getters.data.budgeting);
+      dispatch('updateBudgetingData', getters.data.budgeting, { root: true });
     };
 
     const addon = new Addon({});
@@ -36,14 +39,14 @@ const actions = {
     commit(types.INIT_ADDON, { addon });
   },
 
-  updateData({
-    commit, /* dispatch, */ getters, state,
+  updateAddonData({
+    commit, dispatch, getters, state,
   }, data = {}) {
     return new Promise((resolve, reject) => {
       getters.addon.saveData(data).then((response) => {
         const newOptions = _.merge({}, state.addonOptions, { data });
         commit(types.UPDATE_ADDON_OPTIONS, newOptions);
-        // dispatch('updateBudgeting', data)
+        dispatch('updateBudgetingData', data);
         resolve(response);
       }).catch((err) => reject(err));
     });
