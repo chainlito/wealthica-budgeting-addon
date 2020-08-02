9f54a0915d13b47799292055b9bdbf40c51db252
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-08-02 15:34:34 -0300

    vuetify plugin

--- src/config.js
+++ src/config.js
@@ -1,12 +1,7 @@
 import Vue from 'vue';
-import Polyglot from 'vue-polyglot';
 import App from './components/ConfigWidget.vue';
 
 Vue.config.productionTip = false;
-Vue.use(Polyglot, {
-  defaultLanguage: 'en',
-  languagesAvailable: ['en', 'fr'],
-});
 
 const vueApp = new Vue({
   el: '#app',
--- src/main.js
+++ src/main.js
@@ -1,31 +1,23 @@
 import Vue from 'vue';
-import Polyglot from 'vue-polyglot';
-import KProgress from 'k-progress';
 import { BootstrapVue, BootstrapVueIcons, BIcon } from 'bootstrap-vue';
 import { Plugin } from 'vue-fragment';
 import 'bootstrap-vue/dist/bootstrap-vue-icons.min.css';
 
+import vuetify from './plugins/vuetify';
 import App from './components/MainWidget.vue';
 import store from './store';
 
-Vue.use(Polyglot, {
-  defaultLanguage: 'en',
-  languagesAvailable: ['en', 'fr'],
-});
 Vue.use(Plugin);
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
-Vue.component('k-progress', KProgress);
 Vue.component('b-icon', BIcon);
+
+store.dispatch('initAddon');
 
 const vueApp = new Vue({
   el: '#app',
   store,
-  watch: {
-    language(lang) {
-      this.$polyglot.setLang({ lang });
-    },
-  },
+  vuetify,
   render: (h) => h(App),
 });
 
