87aac610ebd1647900cbf8a7aa2da207812319a6
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-08-03 12:17:50 -0300

    bootstrap

--- src/plugins/bootstrap.js
+++ src/plugins/bootstrap.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import { BootstrapVue, BootstrapVueIcons, BIcon } from 'bootstrap-vue';
 import 'bootstrap-vue/dist/bootstrap-vue-icons.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
