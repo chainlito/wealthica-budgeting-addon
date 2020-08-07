67cb9a4bd565072587e353d78f320cccd917510f
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-08-07 15:57:56 -0300

    regex

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,10 +2,11 @@ import moment from 'moment';
 
 export const formatPrice = (amount, truncate = false) => {
   const number = truncate ? Math.round(amount * 100) / 100 : amount;
-  const string = number.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+  /* const string = number.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
   if (string[0] === '-') {
     return `-$${string.substr(1)}`;
-  }
+  } */
+  const string = number.toString();
 
   return `$${string}`;
 };
